7d0b40d59a007457d89e6c630df5c56aba61a737
ChipDamage <Alex.Heneisen@gmail.com>
2017-10-18 18:06:56 -0400

    Group Work 10/18

---
+++ java.js
@@ -0,0 +1,33 @@
+$(function() {
+  
+  var names = [
+    'Amazon', 
+    'Barnes and Noble', 
+    'Chegg',
+    'ValoreBooks',
+    '2nd and Charles', 
+    'alibris',
+    'ThriftBooks'
+    ];
+    
+  function chooseToExclude(arr) {
+    var x = Math.floor(Math.random() * 5);
+    var array = arr;
+    array.splice(x, 1);
+    return array;
+  }
+  
+  function generatePrice() {
+   var x = Math.random().toFixed(4);
+   x = x * 100;
+   return x;
+  }
+  
+  var names_fake = names;
+  var names_real = chooseToExclude(names_fake);
+  for (var i = 0; i < names_real.length; i++) {
+    var currentName = names_real[i];
+    var currentPrice = generatePrice();
+    $('#sourcesTable').append('<tr><td>' + currentName + '</td><td>' + currentPrice + '</td></tr>');
+  }
+});
